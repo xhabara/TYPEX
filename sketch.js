70b0ff7b28fe138ea41cd8567b81228fb2a8c27a
Xhabara <91153116+xhabara@users.noreply.github.com>
2023-08-18 18:13:19 +0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,10 +5,15 @@ let delayActive = false;
 let loopIndex = 0;
 let characters = [];
 let duration = 100;
-let delay;  // this will hold the delay effect
+let delay;  
+let sequenceButton, playSequenceButton;
+let sequence = [];
+let playingSequence = false; 
+let currentLoopIndex = 0;
+
 
 function preload() {
-  // preload all sounds, use your own sound files here
+  
   sounds['KeyA'] = loadSound('a.wav');
   sounds['KeyB'] = loadSound('b.mp3');
   sounds['KeyC'] = loadSound('c.mp3');
@@ -36,91 +41,186 @@ function preload() {
   sounds['KeyY'] = loadSound('y.mp3');
   sounds['KeyZ'] = loadSound('z.mp3');
   
-  // ... add as many keys as you want, one for each key on your keyboard
+  
 }
 
 function setup() {
-  createCanvas(250, 300);
+  createCanvas(250, 800);
+  background('#202020');
   let input = createInput();
   input.position(20, 65);
 
   input.input(function() {
-    // when something is typed, update the array of characters
     characters = input.value().toUpperCase().split('');
-    // find the key pressed
     let keyPressed = characters[characters.length - 1];
-    // if that key has a sound assigned, play it
     if (sounds[`Key${keyPressed}`]) {
       sounds[`Key${keyPressed}`].play();
     }
   });
 
-  loopButton = createButton('Loop');
-  loopButton.position(20, 100);
-  loopButton.mousePressed(function() {
-    // start looping from the first character
-    loopIndex = 0;
-    looping = true;
-    loopSounds();  // start the loop
-  });
 
-  stopButton = createButton('Stop');
-  stopButton.position(90, 100);
-  stopButton.mousePressed(function() {
-    // stop the loop
+loopButton = createButton('Play');
+loopButton.position(20, 100);
+loopButton.mousePressed(function() {
+  if (looping) {
     looping = false;
-    // stop all sounds
+  
     for (let key in sounds) {
       sounds[key].stop();
     }
-  });
+    loopButton.html('Play'); 
+  } else {
+    
+    loopIndex = 0;
+    looping = true;
+    loopSounds(); 
+    loopButton.html('Stop'); 
+  }
+});
 
-  delaySlider = createSlider(0, 1000, 2000);
-  delaySlider.position(20, 170);
+
+  delaySlider = createSlider(0, 500, 50);
+  delaySlider.position(20, 190);
+  delaySlider.style('width', '200px'); 
+  delaySlider.style('background-color', '#FF5722'); 
+  delaySlider.style('-webkit-slider-thumb', 'background-color: #009688; width: 20px; height: 20px;');
 
   delayEffectButton = createButton('Activate Delay');
-  delayEffectButton.position(20, 190);
+  delayEffectButton.position(118, 100);
   delayEffectButton.mousePressed(function() {
-    delayActive = !delayActive;  // flip the delayActive boolean
+    delayActive = !delayActive;  
     delayEffectButton.html(delayActive ? 'Deactivate Delay' : 'Activate Delay');  // update the button text
   });
 
   // set up the delay effect
   delay = new p5.Delay();
-  delay.setType('feedback'); // a stereo effect
+  delay.setType('feedback'); 
+  
+
+sequenceButton = createButton('Add to Sequence');
+sequenceButton.position(20, 230);
+sequenceButton.style('background-color', '#009688'); 
+sequenceButton.mousePressed(function() {
+  // Add the current loop to the sequence
+  sequence.splice(currentLoopIndex + 1, 0, [...characters]); 
+  console.log('Current sequence:', sequence);  
+});
+
+
+playSequenceButton = createButton('Play Sequence');
+playSequenceButton.position(20, 280);
+playSequenceButton.style('background-color', '#FF5722'); 
+playSequenceButton.mousePressed(function() {
+  playingSequence = !playingSequence; 
+  if (playingSequence) {
+    playSequenceButton.html('Stop Sequence'); 
+    playSequence(0); 
+  } else {
+    playSequenceButton.html('Play Sequence'); 
+    stopAllSounds(); 
+  }
+});
+
+
+  
+
 }
 
+
 function draw() {
-  background(220);
-  text('TYPEX', 12, 50);
-  text(20, 160);
+  background('#202020');
+
+  // Title
+  fill('#F6F1F2');
+  textSize(10);
+  textFont('Courier New');
+  text('TYPEX 1.2', 12, 40);
+
+  
+  let lines = [];
+  let line = 'Sequence: ';
+  for (let i = 0; i < sequence.length; i++) {
+    let nextChunk = sequence[i].join('') + (i < sequence.length - 1 ? ' | ' : '');
+    if (textWidth(line + nextChunk) > width - 20) {
+      lines.push(line);
+      line = nextChunk;
+    } else {
+      line += nextChunk;
+    }
+  }
+  lines.push(line); 
+
+  // Display each line
+  for (let i = 0; i < lines.length; i++) {
+    text(lines[i], 12, 400 + i * 20); 
+  }
+
+
+  fill('#FFFFFF');
+  textSize(12);
+  text('Now Playing: ' + (characters.join('') || 'N/A'), 12, 400 + lines.length * 20);
 }
-function loopSounds() {
-  if (looping && characters[loopIndex]) {
-    // stop all sounds before playing the new one
+
+
+  function loopSounds(callback) {
+  if (looping && loopIndex < characters.length) { 
     for (let key in sounds) {
       sounds[key].stop();
     }
 
-    if (characters[loopIndex] !== ' ') {  // if the character is not a space
-      // play the sound for the current character
+    if (characters[loopIndex] !== ' ') {  
       let soundToPlay = sounds[`Key${characters[loopIndex]}`];
       if (soundToPlay) {
         soundToPlay.play();
-        // if delay is active, connect the sound to the delay effect
+        
         if (delayActive) {
           delay.process(soundToPlay, 0.5, 0.6, 3300);
         } else {
-          // otherwise, set feedback and delay time to 0
+          
           delay.feedback(0);
           delay.delayTime(0);
         }
       }
     }
 
-    // move to the next character, or loop back to the start
-    loopIndex = (loopIndex + 1) % characters.length;
-    // wait for the duration of the sound plus the slider value before playing the next one
-    setTimeout(loopSounds, duration + delaySlider.value());
+    loopIndex++; 
+    setTimeout(function() {
+      loopSounds(callback);
+    }, duration + delaySlider.value());
+  } else if (callback) { 
+    callback(); 
+  }
+}
+
+
+
+function playLoop(loop, index, callback) {
+  currentLoopIndex = index; 
+  characters = loop;
+  loopIndex = 0;
+  looping = true;
+  loopSounds(function() {
+    looping = false;
+    characters = []; 
+    callback(index + 1);
+  });
+}
+
+function playSequence(index) {
+  if (playingSequence) { 
+    if (index < sequence.length) {
+      playLoop(sequence[index], index, playSequence);
+    } else {
+      console.log('Sequence completed, looping now');
+      playSequence(0); 
+    }
+  }
+}
+
+
+function stopAllSounds() {
+  looping = false; 
+  for (let key in sounds) {
+    sounds[key].stop();
   }
 }
